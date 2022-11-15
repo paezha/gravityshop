1aea5ce42fea64d8102c3745d821ad76bff4be97
Antonio Paez <paezha@gmail.com>
2022-11-15 16:02:32 +0100

    Fixed issue with _pkgdown.yml that had residual information from edashop

--- R/gravityshop-package.R
+++ R/gravityshop-package.R
@@ -34,3 +34,81 @@ NULL
 gs_session_1 <- function(){
   writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
 }
+
+#' Spatial Interaction Analysis Workshop Session 2 Template.
+#'
+#' Template for creating a computational notebook for Session 2 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_2
+#' @export
+gs_session_2 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
+
+#' Spatial Interaction Analysis Workshop Session 3 Template.
+#'
+#' Template for creating a computational notebook for Session 3 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_3
+#' @export
+gs_session_3 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
+
+#' Spatial Interaction Analysis Workshop Session 4 Template.
+#'
+#' Template for creating a computational notebook for Session 4 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_4
+#' @export
+gs_session_4 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
+
+#' Spatial Interaction Analysis Workshop Session 5 Template.
+#'
+#' Template for creating a computational notebook for Session 5 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_5
+#' @export
+gs_session_5 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
+
+#' Spatial Interaction Analysis Workshop Session 6 Template.
+#'
+#' Template for creating a computational notebook for Session 6 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_6
+#' @export
+gs_session_6 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
+
+#' Spatial Interaction Analysis Workshop Session 7 Template.
+#'
+#' Template for creating a computational notebook for Session 7 of the workshop.
+#'
+#' @format An R Markdown file
+#'
+#' @keywords datasets
+#' @name gs_session_7
+#' @export
+gs_session_7 <- function(){
+  writeLines("This template is available for use when creating a new R Markdown document;\n please select gs_session_1 from the menu")
+}
